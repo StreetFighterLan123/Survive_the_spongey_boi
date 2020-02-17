4eaf8c7fe3b626297da907d9df9d581017e62def
Omar Ansari <38983519+StreetFighterLan123@users.noreply.github.com>
2020-02-17 00:12:21 -0600

    Added Files

---
+++ main.py
@@ -0,0 +1,93 @@
+import pygame
+import random
+import math
+import time
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Survive the Spongey Boi")
+
+#Player
+playerImg = pygame.image.load('monster.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+playerY_change = 0
+
+#Spongebob
+enemyImg = pygame.image.load('sponge.png')
+enemyX = random.randint(0,735)
+enemyY = random.randint(10,400)
+enemyX_change = 0
+enemyY_change = 0
+
+#Font for Game Over
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def player(x,y):
+    screen.blit(playerImg, (x,y))
+
+def enemy(x,y):
+    screen.blit(enemyImg, (x,y))
+
+def isCollision(enemyX, enemyY, playerX, playerY):
+    #Distance formula in Python
+    distance = math.sqrt((math.pow(enemyX - playerX,2)) + (math.pow(enemyY - playerY,2))) 
+    if distance < 27:
+        return True
+    else: 
+        return False
+    
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (0, 0, 0))
+    screen.blit(over_text, (200,250))
+
+#Game loop
+running = True
+
+while running:
+    screen.fill((255,255,204))
+    #Do the for loop here.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        #Do the keydown
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 2
+            if event.key == pygame.K_UP:
+                playerY_change = -2
+            if event.key == pygame.K_DOWN:
+                playerY_change = 2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+        
+
+    playerX += playerX_change
+    playerY += playerY_change
+    #Boundaries
+    if playerX <= 0:
+        playerX = 0
+    elif playerX > 736:
+        playerX = 736
+    if playerY <= 0:
+        playerY = 0
+    elif playerY > 536:
+        playerY = 536
+
+    if isCollision(enemyX, enemyY, playerX, playerY):
+        game_over_text()
+        playerX = 10000
+        enemyX = 10000
+        time.sleep(1)
+        break
+        
+        
+    enemy(enemyX, enemyY)
+    player(playerX, playerY)
+    pygame.display.update()
